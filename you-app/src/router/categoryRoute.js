cb72b25b5d0b83b099c7c3b74a5327cbdd7b7792
liu1410121464 <1410121464@qq.com>
2019-02-12 20:23:25 +0800

    分类路由

--- you-app/src/router/categoryRoute.js
+++ you-app/src/router/categoryRoute.js
@@ -1,5 +1,22 @@
 const route = {
-    path:'/category',
-    component:()=>import('../pages/category/index')
+    path: '/category',
+    component: () => import('../pages/category/index'),
+    children: [
+        {
+            path: 'group/:id',
+            component: () => import('../pages/category/category-group'),
+            props: true
+        },
+        {
+            path: 'home',
+            component: () => import('../pages/home'),
+            props: true
+        },
+        {
+            path: 'search',
+            props: true,
+            component: () => import('../pages/home/search')
+        },
+    ]
 }
 export default route;
